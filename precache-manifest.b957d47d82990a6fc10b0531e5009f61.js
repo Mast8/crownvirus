bf9fec263b0b0f70f3aa4a89fb573ee9a3efe4ff
mastelo <mirandarodriguezronald@gmail.com>
2020-09-11 19:02:42 -0400

    Updates

---
+++ precache-manifest.b957d47d82990a6fc10b0531e5009f61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9414e4503af58804e914a11c18a88c",
+    "url": "/crownvirus/index.html"
+  },
+  {
+    "revision": "ab4a15e5648dbe322644",
+    "url": "/crownvirus/static/css/main.6c382273.chunk.css"
+  },
+  {
+    "revision": "8fbaba8980f7a765c5ec",
+    "url": "/crownvirus/static/js/2.708667fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/crownvirus/static/js/2.708667fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4a15e5648dbe322644",
+    "url": "/crownvirus/static/js/main.f86194b4.chunk.js"
+  },
+  {
+    "revision": "be638e41823eaf183c26",
+    "url": "/crownvirus/static/js/runtime-main.647fa69a.js"
+  },
+  {
+    "revision": "ff9e669e0845f96167d8c32156384625",
+    "url": "/crownvirus/static/media/corona.ff9e669e.png"
+  }
+]);
